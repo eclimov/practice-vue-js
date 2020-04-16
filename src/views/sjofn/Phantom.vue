b06703a6ad2e75cdd4fbc5d8eba659834538b8b9
Tatiana Zatushevskaya <tzatusevscaia@pentalog.com>
2020-04-16 22:52:28 +0300

    - added necessary directives

--- src/views/sjofn/Phantom.vue
+++ src/views/sjofn/Phantom.vue
@@ -1,8 +1,18 @@
 <template>
   <div>
-    <!--Show link to Google if isGoogle. Show link to Yandex otherwise-->
+    <h1 v-if="isGoogle">
+      <a href="https://www.google.com/">link to Google</a>
+    </h1>
+    <h1 v-else>
+      <a href="https://www.yandex.ru/">link to Yandex</a>
+    </h1>
 
-    <!--Enumerate Agile Club members here-->
+    <h1
+      v-for="agileClubMember in agileClubMembers"
+      :key="agileClubMember"
+    >
+      {{ agileClubMember }}
+    </h1>
   </div>
 </template>
 
@@ -31,5 +41,10 @@ export default {
     font-family: "Blackadder ITC",fantasy;
     font-size: 50px;
     color: #732222;
+  }
+  h1 {
+    font-family: Arial;
+    font-size: 50px;
+    color: black;
   }
 </style>
