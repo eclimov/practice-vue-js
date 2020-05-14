78182151036940f4f902f9b0eca0cff8bcf24e5e
Sanda Panteleimonov <spanteleimonov@pentalog.com>
2020-05-14 10:49:58 +0300

    Added new Vue.js views and components. Added header / footer as components.

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,5 +1,13 @@
-<template />
-
+<template>
+  <div class="footer">
+    <h2>IT-Sanda</h2>
+    <h4>Contact information:</h4>
+    <address style="text-align: left;">
+      Created by <a href="mailto:itsanda@gmail.com">IT-Sanda</a>.<br>
+      Visit our office at:<br><br>
+    </address>
+  </div>
+</template>
 <script>
 export default {
   name: 'Footer'
@@ -7,5 +15,16 @@ export default {
 </script>
 
 <style scoped>
-
+  .footer {
+    display: block;
+    height: auto;
+    width: 100%;
+    padding: 20px;
+    margin-top: 20px;
+    font-family: Laila, serif;
+    font-style: normal;
+    font-variant: normal;
+    text-align: left;
+    background: #af7ca0;
+  }
 </style>
